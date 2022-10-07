f9924885b43acbedf2d24cc4cf9993766e84023a
fortuna <avidia38@gmail.com>
2022-10-07 11:31:33 +0900

    feat: stock_api_test

--- lib/data/source/remote/stock_api.dart
+++ lib/data/source/remote/stock_api.dart
@@ -1,16 +1,17 @@
 import 'package:http/http.dart' as http;
+
 class StockApi {
   static const baseUrl = 'https://www.alphavantage.co';
   static const apiKey = 'F1R68TCH0OSLYQ6T';
 
-  final http.Client _client;
-
-  StockApi({http.Client? client}) : _client = (client ?? http.Client());
+  final http.Client client;
+  // client가 null이면 : client...을 불러 기본 Client불러온다. test 코드작성용
+  StockApi({http.Client? client}) : client = (client ?? http.Client());
   // csv 파일 호출
   Future<http.Response> getListings({String apiKey = apiKey}) async {
-    return await _client.get(
-      Uri.parse('$baseUrl/query?function=LISTING_STATUS&apiKey=$apiKey')
+    return await client.get(
+      Uri.parse('$baseUrl/query?function=LISTING_STATUS&apikey=$apiKey')
     );
   }
-
+// https://apikey : lowercase
 }
